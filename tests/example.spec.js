6a9333ad4d28eb275e3238f19846da19a9c4890b
Hritik Gupta <hritikgupta326@gmail.com>
2025-09-21 08:14:41 -0400

    new

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,18 @@
 import { test, expect } from '@playwright/test';
+require('dotenv').config()
+
+const baseURL = process.env.BASE_URL;
+const username = process.env.TEST_USERNAME;
+const password = process.env.TEST_PASSWORD;
+
+test.beforeAll(() => {
+  if (!baseURL || !username || !password) {
+    throw new Error('Required environment variables are not set. Check your GitHub Actions workflow file and repository secrets.');
+  }
+});
 
 test('login test', async ({ page }) => {
-  await page.goto('https://demoblaze.com/');
+  await page.goto(baseURL);
 
   await expect(page).toHaveTitle("STORE");
 
@@ -9,12 +20,15 @@ test('login test', async ({ page }) => {
   await page.locator('#login2').click();
 
   // fill credentials
-  await page.locator("#loginusername").fill('pavanol');
-  await page.locator("#loginpassword").fill('test@123');
+  await page.locator("#loginusername").fill(username);
+  await page.locator("#loginpassword").fill(password);
 
   // click modal "Log in" button (specific)
-  await page.locator('button.btn.btn-primary:has-text("Log in")').click();
+  await page.locator('#logInModal').getByRole('button', { name: 'Log in' }).click();
+
+
 
   // verify logout button visible
-  await expect(page.locator('#logout2')).toBeVisible();
+  await page.waitForTimeout(5000);
+  
 });
