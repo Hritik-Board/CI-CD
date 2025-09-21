cbdf0214d43107246db549a3a83ac72ce1a6820e
Hritik Gupta <hritikgupta326@gmail.com>
2025-09-21 07:37:56 -0400

    new

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,10 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('login test', async ({ page }) => {
   await page.goto('https://demoblaze.com/');
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("STORE");
+
+  // open login modal
+  await page.locator('#login2').click();
+
+  // fill credentials
+  await page.locator("#loginusername").fill('pavanol');
+  await page.locator("#loginpassword").fill('test@123');
+
+  // click modal "Log in" button (specific)
+  await page.locator('button.btn.btn-primary:has-text("Log in")').click();
+
+  // verify logout button visible
+  await expect(page.locator('#logout2')).toBeVisible();
 });
-
-
